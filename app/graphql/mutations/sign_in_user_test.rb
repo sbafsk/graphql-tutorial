5f2a1cdc49fb5785c8e2133f2871f7a99c8911bf
sbafsk <sebastian@occupier.com>
2021-12-07 14:19:09 -0300

    typo fix

--- app/graphql/mutations/sign_in_user_test.rb
+++ app/graphql/mutations/sign_in_user_test.rb
@@ -2,26 +2,26 @@ require 'test_helper'
 
 class Mutations::SignInUserTest < ActiveSupport::TestCase
   def perform(args = {})
-    Mutations::SignInUser.new(object: nil, field: nil, context: { sessions: {}}).resolve(args)
+    Mutations::SignInUser.new(object: nil, field: nil, context: { session: {} }).resolve(args)
   end
 
   def create_user
     User.create!(
       name: 'test user',
       email: '123@123.com',
-      password: '[omitted]'
+      password: '123'
     )
   end
 
   test 'success' do
     user = create_user
-
     result = perform(
       credentials: {
         email: user.email,
         password: user.password
       }
     )
+    p 'result ${result}'
 
     assert result[:token].present?
     assert_equal result[:user], user
@@ -33,11 +33,11 @@ class Mutations::SignInUserTest < ActiveSupport::TestCase
 
   test 'failure because wrong email' do
     create_user
-    assert_nil perform(credentials: { email: 'wrong'})
+    assert_nil perform(credentials: { email: 'wrong' })
   end
 
   test 'failure because wrong password' do
     user = create_user
-    assert_nil perform(credentials: { email: user.email, password: 'wrong'})
+    assert_nil perform(credentials: { email: user.email, password: 'wrong' })
   end
 end
